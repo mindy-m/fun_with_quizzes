9dd67d7c634598b602cc410c239be6d9f2a3b6f1
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-23 19:13:06 -0400

    Created base class

---
+++ src/studio/launchcode/Question.java
@@ -0,0 +1,8 @@
+package studio.launchcode;
+
+/* First, design a base class (called Question) that contains the common features,
+   and design subclasses for each of the question types. */
+
+public class Question {
+
+}
