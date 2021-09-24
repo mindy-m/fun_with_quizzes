66600a35208ea419bc26a5e5061044a6d867adfb
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-23 21:08:51 -0400

    Created subclass Classes

---
+++ src/studio/launchcode/Checkbox.java
@@ -0,0 +1,16 @@
+package studio.launchcode;
+
+public class Checkbox extends Question {
+    /* For each question type be sure to include:
+
+    Class name
+    Fields and properties with access modifiers
+    Methods with access modifiers
+    Any inheritance relationship
+
+    Should any of the question classes be abstract? If so, should any of its methods be abstract?
+
+    Make sure that there is functionality included to display the questions, to display the
+    possible answers, and to check to see if the answer(s) is correct.
+     */
+}
